23bbeaecff41edf351c871facfd5d19c69a92983
Mauricio Marin-Cabrera <mmmarinc.amaz@gmail.com>
2023-04-30 21:25:38 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// Write your code here!
+
+
+document.getElementById('main').remove();
+
+const newHeader = document.createElement('H1');
+newHeader.id = "victory";
+
+const YOUR_NAME = "Mohammad Elyas";
+newHeader.innerHTML = `${YOUR_NAME} is the champion`
